03a1f612b615ce8232e324387b122e3a8defa5a5
BloodLind <pashtet.sadist@gmail.com>
2021-08-25 16:42:38 +0300

    Add Start Projects

---
+++ BicycleStore/BicycleStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BicycleStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
